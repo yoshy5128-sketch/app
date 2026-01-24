a02ef6a0a9c01ee10c5fe0cdd75c7f32515fdf1b
Yoshy <yoshy5128@gmail.com>
2026-01-24 14:16:05 +0900

    Add files via upload

---
+++ eve/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'everest-sim-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './top.png',
+  './icon.png',
+  './wind.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
