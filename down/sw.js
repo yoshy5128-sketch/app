0a6e7f1068aef40e443a1e740fcbd0864a074294
Yoshy <yoshy5128@gmail.com>
2026-02-01 09:32:39 +0900

    Add files via upload

---
+++ down/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'descent-maze-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
