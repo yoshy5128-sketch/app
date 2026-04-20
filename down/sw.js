9e6fd437e21e446389a14f282754a4bc8347c5d3
Yoshy <yoshy5128@gmail.com>
2026-04-20 19:12:48 +0900

    Add files via upload

--- down/sw.js
+++ down/sw.js
@@ -1,17 +1,33 @@
-const CACHE_NAME = 'descent-maze-v6';
+const CACHE_NAME = 'descent-maze-v7';
 const ASSETS = [
-  './',
   './index.html',
-  './icon-512.png'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './wind.mp3',
+  './get.mp3',
+  './gan.mp3',
+  './clear.mp3',
+  './ashi.mp3',
+  './chaku.mp3'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
   );
